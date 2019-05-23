799cf30c8ca38209c2d5c5a164ed4d3f305c5041
Sean McPherson <mail@seanmcp.com>
2019-05-22 23:14:12 -0400

    Rename checker -> check

---
+++ check/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 8000;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (_, res) => res.send("Hello from raedan-check"));
+
+app.post("/", (req, res) => {
+  if (!req.body) {
+    res.send("Error: You must send a body to `/`");
+  }
+  res.send(`Received ${JSON.stringify(req.body)}`);
+});
+
+app.listen(PORT, () => console.log(`raedan-check listening on port ${PORT}`));
